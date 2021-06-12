dd4ce692ec24078e448ee804e02c49a4a8140d9d
Tester ID <85047938+tester-id@users.noreply.github.com>
2021-06-12 20:05:39 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6281515860089','6281334177340'] // Put your number here
+global.owner = ['6281246897183', '6287758845234'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -13,7 +13,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'I hope you\'re fine'
-global.author = 'Nurutomo'
+global.author = 'Tester-ID'
 
 global.multiplier = 69 // The higher, The harder levelup
 
